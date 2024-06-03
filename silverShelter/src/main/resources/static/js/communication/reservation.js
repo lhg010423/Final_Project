a2401a0933b9cb35fa73f0094760b3bd342a016e
gorru135 <gggorru@gmail.com>
2024-06-03 17:21:19 +0900

    24-06-03

--- silverShelter/src/main/resources/static/js/communication/reservation.js
+++ silverShelter/src/main/resources/static/js/communication/reservation.js
@@ -1,12 +1,1 @@
 
-document.addEventListener('DOMContentLoaded',function() {
-    var calendarEl = document.getElementById('calendar')
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        headerToolbar: {
-            left:'prevYear,prev,next,nextYear today',
-            center: 'title',
-            right:'dayGridMonth,dayGridWeek,dayGridDay'
-        }
-    })
-})
