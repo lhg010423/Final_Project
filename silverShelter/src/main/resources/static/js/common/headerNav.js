49070c3ebd389c932d5588c684bd9cb7fb860349
saemh95 <saem_hong_kr@hotmail.com>
2024-06-03 18:31:40 +0900

    240603repair

---
+++ silverShelter/src/main/resources/static/js/common/headerNav.js
@@ -0,0 +1,26 @@
+/* 내정보 드랍다운 */
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var memberName = document.getElementById("memberName");
+    var logonDropdown = document.getElementById("logonDropdown");
+
+    memberName.onmouseover = function() {
+        logonDropdown.style.display = "block";
+    }
+
+    memberName.onmouseout = function() {
+        setTimeout(function() {
+            if (!logonDropdown.matches(':hover')) {
+                logonDropdown.style.display = "none";
+            }
+        }, 250);
+    }
+
+    logonDropdown.onmouseover = function() {
+        logonDropdown.style.display = "block";
+    }
+
+    logonDropdown.onmouseout = function() {
+        logonDropdown.style.display = "none";
+    }
+});
