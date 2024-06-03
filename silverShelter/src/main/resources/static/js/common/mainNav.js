6c88d37c641b6c509fb1bffe4c4a86a1609ee03c
saemh95 <saem_hong_kr@hotmail.com>
2024-06-03 11:20:28 +0900

    240603MainRepair

--- silverShelter/src/main/resources/static/js/common/mainNav.js
+++ silverShelter/src/main/resources/static/js/common/mainNav.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    document.querySelector('.hidden-bar').addEventListener('click', function() {
+    document.querySelector('.hidden-bar').addEventListener('mouseenter', function() {
 
         	document.getElementById('sideNav').classList.toggle('active');
     });
 
 
-        document.querySelector('.closebtn').addEventListener('click', function() {
+       /* document.querySelector('.closebtn').addEventListener('click', function() {
 
         document.getElementById('sideNav').classList.remove('active');
 
-    	});
+    	});*/
+    	
+    	sideNav.addEventListener('mouseleave', function() {
+        sideNav.classList.remove('active');
+    });
 });
