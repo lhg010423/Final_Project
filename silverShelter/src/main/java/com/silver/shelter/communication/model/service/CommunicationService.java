5e3d22bbf26a919a1f421fe2081a6b27a6648cf0
gorru135 <gggorru@gmail.com>
2024-06-10 17:29:47 +0900

    24-06-10

--- silverShelter/src/main/java/com/silver/shelter/communication/model/service/CommunicationService.java
+++ silverShelter/src/main/java/com/silver/shelter/communication/model/service/CommunicationService.java
@@ -4,17 +4,17 @@ import com.silver.shelter.clubReservation.model.dto.ClubReservation;
 
 public interface CommunicationService {
 
-	/** 게스트룸 예약하기 
+	/** 예약하기 
 	 * @param reservation
 	 * @return
 	 */
-	int guestRoomReservation(ClubReservation reservation);
+	int reservation(ClubReservation reservation);
 
 	/** 예약 시간 겹치는지 확인
 	 * @param reservation
 	 * @return
 	 */
-	int selectGuestRoomReservation(ClubReservation reservation);
+	int selectReservation(ClubReservation reservation);
 
 
 }
