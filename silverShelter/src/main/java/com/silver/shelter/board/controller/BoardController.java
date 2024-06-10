0da76756d0f4d22d1a3ea39226f46fd4b06d271d
lhg010423 <lhg010423@gmail.com>
2024-06-10 17:31:00 +0900

    게시굴 상세조회

--- silverShelter/src/main/java/com/silver/shelter/board/controller/BoardController.java
+++ silverShelter/src/main/java/com/silver/shelter/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.silver.shelter.board.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.silver.shelter.board.model.service.BoardService;
+import com.silver.shelter.member.model.dto.Member;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -66,6 +70,62 @@ public class BoardController {
 		
 		return "board/boardList";
 	}
+	
+	
+	
+	/** 게시글 상세보기
+	 * @param boardCode : 공지, 자유, 문의 게시판 구분용
+	 * @param boardNo : 게시글 번호
+	 * @param cp : 목록으로 버튼 클릭시 전 페이지로 돌아가기 위해 씀
+	 * @param paramMap : 검색했을 때 목록으로 버튼 클릭시 key, query값으로
+	 * 					 전페이지로 돌아가기 위해 씀
+	 * @param loginMember : 로그인한 회원확인용
+	 * @param model : 데이터 전달용
+	 * @return
+	 */
+	@GetMapping("{boardCode:[0-9]+}/{boardNo:[0-9]+}")
+	public String boardDetail(
+			@PathVariable("boardCode") int boardCode,
+			@PathVariable("boardNo") int boardNo,
+			@RequestParam(value="cp", required = false, defaultValue = "1") int cp,
+			@RequestParam Map<String, Object> paramMap,
+			@SessionAttribute(value="loginMember", required = false) Member loginMember,
+			HttpSession session,
+			Model model
+			) {
+		
+		// 데이터 전달용 객체 생성
+		Map<String, Object> map = new HashMap<>();
+		map.put("boardCode", boardCode);
+		map.put("boardNo", boardNo);
+		if(loginMember != null) {
+			map.put("memberNo", loginMember.getMemberNo());			
+		}
+		
+		
+		Map<String, Object> boardDetail = service.boardDetail(map);
+		
+		
+		// 검색 쿼리 및 페이지 번호 저장(목록으로 버튼 클릭 시 사용할 것)
+		session.setAttribute("key", paramMap.get("key"));
+		session.setAttribute("query", paramMap.get("query"));
+		session.setAttribute("cp", cp);
+		
+		
+		model.addAttribute("boardList", boardDetail.get("boardList"));
+		model.addAttribute("canModify", boardDetail.get("canModify"));
+		model.addAttribute("canDelete", boardDetail.get("canDelete"));
+		
+		
+		return "board/boardDetail";
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
