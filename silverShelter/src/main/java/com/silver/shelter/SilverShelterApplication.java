83fde80471e01693c5aacf28cfb3f347c2f95b02
choiyunjung88 <lucy023@sookmyung.ac.kr>
2024-06-12 12:41:27 +0900

    06/12 choiyunjung

--- silverShelter/src/main/java/com/silver/shelter/SilverShelterApplication.java
+++ silverShelter/src/main/java/com/silver/shelter/SilverShelterApplication.java
@@ -1,15 +1,25 @@
 package com.silver.shelter;
 
+import java.io.IOException;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+import com.silver.shelter.careGiver.clustering.KMeansClustering;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SilverShelterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SilverShelterApplication.class, args);
+        try {
+            KMeansClustering.main(args);
+        } catch (IOException e) {
+            System.err.println("An error occurred while running KMeansClustering: " + e.getMessage());
+            e.printStackTrace();
+        }
 	}
 
 }
--- silverShelter/src/main/java/com/silver/shelter/careGiver/service/CareGiverService.java
+++ silverShelter/src/main/java/com/silver/shelter/careGiver/service/CareGiverService.java
@@ -1,39 +1,19 @@
 package com.silver.shelter.careGiver.service;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import java.util.List;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
 import com.silver.shelter.careGiver.model.CareGiver;
+import com.silver.shelter.careGiver.model.CaregiverCluster;
 
 @Service
 public class CareGiverService {
-//	public String getRecommendation(String gender, int age, int experience, String workingHours, String roles) {
-//        RestTemplate restTemplate = new RestTemplate();
-//        String url = "http://localhost:5000/predict";
-//
-//        JSONObject requestJson = new JSONObject();
-//        requestJson.put("gender", gender);
-//        requestJson.put("age", age);
-//        requestJson.put("experience", experience);
-//        requestJson.put("workingHours", workingHours);
-//        requestJson.put("roles", roles);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        HttpEntity<String> entity = new HttpEntity<>(requestJson.toString(), headers);
-//        ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
-//
-//        return response.getBody();
-//    }
-//
-//	public String getRecommendation(String formData) {
-//		// 요양사 추천 로직 구현
-//        // AI 모델 호출 및 결과 처리 등
-//        return "Best Caregiver recommendation based on the survey data";
-//	}
+    private List<CaregiverCluster> clusters;
+
+    public CareGiverService(List<CaregiverCluster> clusters) {
+        this.clusters = clusters;
+    }
+
+    public List<CareGiver> recommendCaregivers(CareGiver userData) {
+        // 사용자 정보를 기반으로 요양사 추천
+        return null;
+    }
 }
