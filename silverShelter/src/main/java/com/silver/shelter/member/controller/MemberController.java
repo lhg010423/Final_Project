edf4b2ca56ab543d1a1807f7ef62af5f0bb8a654
choiyunjung88 <lucy023@sookmyung.ac.kr>
2024-06-12 12:58:33 +0900

    06/12

--- silverShelter/src/main/java/com/silver/shelter/member/controller/MemberController.java
+++ silverShelter/src/main/java/com/silver/shelter/member/controller/MemberController.java
@@ -32,7 +32,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @SessionAttributes({"loginMember"})
 @RequestMapping("member")
-@Slf4j
 public class MemberController {
 
 	private final MemberService service;
