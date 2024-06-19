d260f25e3d8d9a77b9b07b689dc63403750796ad
lhg010423 <lhg010423@gmail.com>
2024-06-19 17:41:46 +0900

    관리자 게시판

--- silverShelter/src/main/java/com/silver/shelter/board/controller/CommentController.java
+++ silverShelter/src/main/java/com/silver/shelter/board/controller/CommentController.java
@@ -2,7 +2,11 @@ package com.silver.shelter.board.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +24,48 @@ public class CommentController {
 	private final CommentService service;
 	
 	
+	/** 댓글 목록 조회
+	 * @param boardNo
+	 * @return
+	 */
 	@GetMapping("")
 	public List<Comment> select(@RequestParam("boardNo") int boardNo) {
 		return service.select(boardNo);
 	}
+	
+	
+	/** 댓글, 대댓글 작성
+	 * @param comment
+	 * @return
+	 */
+	@PostMapping("")
+	public int insert(@RequestBody Comment comment) {
+		return service.insert(comment);
+	}
+	
+	
+	/** 댓글 삭제
+	 * @param commentNo
+	 * @return
+	 */
+	@DeleteMapping("")
+	public int delete(@RequestBody int commentNo) {
+		return service.delete(commentNo);
+	}
+	
+	
+	/** 댓글 수정
+	 * @param comment
+	 * @return
+	 */
+	@PutMapping("")
+	public int update(@RequestBody Comment comment) {
+		return service.update(comment);
+	}
+	
+	
+	
+	
 	
 	
 	
--- silverShelter/src/main/resources/static/js/admin/admin.js
+++ silverShelter/src/main/resources/static/js/admin/admin.js
@@ -1,48 +1,51 @@
-document.getElementById("memberId").addEventListener("click", e => {
+document.querySelectorAll(".memberId").forEach(button => {
+    button.addEventListener("click", e => {
 
-    // 버튼에 회원 번호 가져오기
-    const memberNo = document.getElementById("memberId").name;
+        // 버튼에 회원 번호 가져오기
+        // const memberNo = document.getElementById("memberId").name;
+        const memberNo = e.target.name;
 
-    console.log(memberNo);
 
-    fetch("/admin/adminSelect", {
-        method : "POST",
-        headers : {"Content-Type" : "application/json"},
-        body : memberNo
+        console.log(memberNo);
+
+        fetch("/admin/adminSelect", {
+            method : "POST",
+            headers : {"Content-Type" : "application/json"},
+            body : memberNo
+        })
+        .then(resp => resp.json())
+        .then(result => {
+
+            if(result == null) {
+                alert("값 없음");
+                e.preventDefault();
+            } else {
+
+                console.log(result);
+
+
+
+                const nameInput = document.getElementById("nameInput");
+                nameInput.innerText = result.memberName;
+
+                const idInput = document.getElementById("idInput");
+                idInput.innerText = result.memberId;
+
+                const telInput = document.getElementById("telInput");
+                telInput.innerText = result.memberTel;
+
+                const gdtelInput = document.getElementById("gdtelInput");
+                gdtelInput.innerText = result.guardianTel;
+                
+                
+                const roomNo = document.getElementById("roomNo");
+                roomNo.innerText = result.roomNo;
+
+            }
+        })
+
     })
-    .then(resp => resp.json())
-    .then(result => {
-
-        if(result == null) {
-            alert("값 없음");
-            e.preventDefault();
-        } else {
-
-            console.log(result);
-
-
-
-            const nameInput = document.getElementById("nameInput");
-            nameInput.innerText = result.memberName;
-
-            const idInput = document.getElementById("idInput");
-            idInput.innerText = result.memberId;
-
-            const telInput = document.getElementById("telInput");
-            telInput.innerText = result.memberTel;
-
-            const gdtelInput = document.getElementById("gdtelInput");
-            gdtelInput.innerText = result.guardianTel;
-            
-            
-            const roomNo = document.getElementById("roomNo");
-            roomNo.innerText = result.roomNo;
-
-        }
-    })
-
 })
-
 
 // 회원 목록 조회 게시글에서 회원버튼을 클릭시 오른쪽 상세조회에 값이 출력되는 함수
 // function memberDetail() {
